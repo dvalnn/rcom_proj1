c64d1479cd30e67fc14cd7913ecf524c73af7fe4
dvalinn <tiago.andre.amorim@gmail.com>
2023-03-19 23:46:05 +0000

    reduced cmd args

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include "app.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Usage: %s url port\n", argv[0]);
+    if (argc < 2) {
+        printf("Usage: %s url\n", argv[0]);
         exit(1);
     }
 
-    return run(argv[1], argv[2]);
+    return run(argv[1]);
 }
