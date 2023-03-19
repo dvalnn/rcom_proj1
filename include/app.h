14d05fcd77d104e704e9208cb38d4fc8ad89e82e
dvalinn <tiago.andre.amorim@gmail.com>
2023-03-19 12:57:39 +0000

    repo init

---
+++ include/app.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef _APP_H_
+#define _APP_H_
+
+int helloWorld();
+
+#endif  // _APP_H_
---
+++ src/app.c
@@ -0,0 +1,6 @@
+#include "app.h"
+
+int helloWorld() {
+    printf("hello world\n");
+    return 0;
+}
